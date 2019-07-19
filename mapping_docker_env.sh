bfd550709bbc1b853c495fcda30e7b4738001202
Zoran Rasic <zokiupwork@gmail.com>
2019-07-19 16:05:19 +0300

    Update mapping_docker_env.sh

--- mapping_docker_env.sh
+++ mapping_docker_env.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 echo ${DEV_AWS_ACCESS_KEY_ID}
-sed -e "s/\${AWS_ACCESS_KEY_ID}/${DEV_AWS_ACCESS_KEY_ID}/g;
-         s/\${AWS_SECRET_ACCESS_KEY}/${DEV_AWS_SECRET_ACCESS_KEY}/g;
-         s/\${AWS_DEFAULT_REGION}/${DEV_AWS_DEFAULT_REGION}/g;"  ./app/.env.deploy > ./app/.env
+sed -e "s/\${AWS_ACCESS_KEY_ID}/${DEV_AWS_ACCESS_KEY_ID}/g;"  ./app/.env.deploy > ./app/.env
